bca0c9aedb5882839509b67df495c3760cfec45e
Simon Rosén <simon.rosen@elev.ga.ntig.se>
2024-02-22 10:36:37 +0100

    nnn

---
+++ slutprojekt/Program.cs
@@ -0,0 +1,21 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+Raylib.InitWindow(1280, 720, "hejhej");
+Raylib.SetTargetFPS(60);
+
+Rectangle rect = new (, 20);
+
+while (!Raylib.WindowShouldClose()){
+    Raylib.BeginDrawing();
+
+    Raylib.ClearBackground(Color.DarkGray);
+
+    Raylib.EndDrawing();
+}
+
+
+class Player{
+    public Vector2 position = new Vector2(0, 0);
+    
+}
